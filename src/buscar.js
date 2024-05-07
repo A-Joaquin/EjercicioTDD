320687cba293339910f2ff44f81504001759c763
arturo <arturo.veliz@ucb.edu.bo>
2024-05-07 13:51:00 -0400

    refactor codigo, se añadio la funcion startsWitch() dentro de buscarProyecto para simplificar codigo

--- src/buscar.js
+++ src/buscar.js
@@ -4,13 +4,10 @@ function buscarProyecto(nombre, proyectos) {
     let coincidencias = [];
 
     for (let proyecto of proyectos) {
-        let countCoincidencias = 0;
-
-        for (let i = 0; i < nombre.length; i++) {
-            if (nombre[i] === proyecto[i]) countCoincidencias++;
+        if(proyecto.startsWith(nombre))
+        {
+            coincidencias.push(proyecto);
         }
-
-        if (countCoincidencias > 0) coincidencias.push(proyecto);
     }
 
     return coincidencias.length > 0 ? coincidencias : "";
